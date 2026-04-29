8cee473e3492d6c9cfdb3e927e2454d7c98c2a3e
Ziad-Khattab <209370232+Ziad-Khattab@users.noreply.github.com>
2026-04-29 17:27:02 +0300

    chore: remove tracked build artifacts and update .gitignore

--- src/main/webapp/_app/immutable/chunks/7KmZNhr_.js
+++
@@ -1,1 +0,0 @@
-import{p as r}from"./DDrYZ3Z9.js";import{s as t}from"./yaJnbZb_.js";const o={get error(){return r.error},get status(){return r.status}};t.updated.check;export{o as p};
--- src/main/webapp/_app/immutable/entry/start.DHupW7G_.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/yaJnbZb_.js";import{x as t}from"../chunks/DDrYZ3Z9.js";export{t as load_css,r as start};
